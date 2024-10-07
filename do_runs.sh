3b00b75fb59214e6a316aec4bd332dfa9adf7368
Marcogarofalo <marcogarofalo31@gmail.com>
2024-10-07 16:30:37 +0200

    bootstraping runs

--- do_runs.sh
+++ do_runs.sh
@@ -1,20 +1,20 @@
 bad=()
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cA.53.24  -bin 50  -L 24  jack -mu 0.00530 || bad+=("cA.53.24")
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cA.40.24  -bin 50  -L 24  jack -mu 0.00400 || bad+=("cA.40.24")
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cA.30.32  -bin 50  -L 32  jack -mu 0.00300 || bad+=("cA.30.32")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cA.53.24  -bin 50  -L 24  jack -mu 0.00530 || bad+=("cA.53.24")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cA.40.24  -bin 50  -L 24  jack -mu 0.00400 || bad+=("cA.40.24")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cA.30.32  -bin 50  -L 32  jack -mu 0.00300 || bad+=("cA.30.32")
 if [  $1 == "first_run" ]
 then
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cB.72.64  -bin 50  -L 64  jack -mu 0.00072 || bad+=("cB.72.64")
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cB.72.96  -bin 50  -L 96  jack -mu 0.00072 || bad+=("cB.72.96")
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cC.06.80  -bin 50  -L 80  jack -mu 0.00060 || bad+=("cC.06.80")
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cC.06.112 -bin 50  -L 112 jack -mu 0.00060 || bad+=("cC.06.112")
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cD.54.96  -bin 50  -L 96  jack -mu 0.00054 || bad+=("cD.54.96") 
-./mpi_fpi read_plateaux -p ../../g-2_new_stat_try/ cE.44.112 -bin 50  -L 112 jack -mu 0.00044 || bad+=("cE.44.112")
-./mpi_fpi  read_plateaux -p ../../g-2_new_stat_try/ cB.14.64  -bin 50  -L 64  jack -mu 0.0014 || bad+=("mpi cB.14.64")
-./mpi_fpi  read_plateaux -p ../../g-2_new_stat_try/ cB.25.48  -bin 50  -L 48  jack -mu 0.0025 || bad+=("mpi cB.25.48")
-./mpi_fpi  read_plateaux -p ../../g-2_new_stat_try/ cC.20.48  -bin 50  -L 48  jack -mu 0.0020 || bad+=("mpi cB.20.48")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cB.72.64  -bin 50  -L 64  jack -mu 0.00072 || bad+=("cB.72.64")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cB.72.96  -bin 50  -L 96  jack -mu 0.00072 || bad+=("cB.72.96")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cC.06.80  -bin 50  -L 80  jack -mu 0.00060 || bad+=("cC.06.80")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cC.06.112 -bin 50  -L 112 jack -mu 0.00060 || bad+=("cC.06.112")
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cD.54.96  -bin 50  -L 96  jack -mu 0.00054 || bad+=("cD.54.96") 
+./mpi_fpi read_plateaux -p ../../g-2_new_stat/ cE.44.112 -bin 50  -L 112 jack -mu 0.00044 || bad+=("cE.44.112")
+./mpi_fpi  read_plateaux -p ../../g-2_new_stat/ cB.14.64  -bin 50  -L 64  jack -mu 0.0014 || bad+=("mpi cB.14.64")
+./mpi_fpi  read_plateaux -p ../../g-2_new_stat/ cB.25.48  -bin 50  -L 48  jack -mu 0.0025 || bad+=("mpi cB.25.48")
+./mpi_fpi  read_plateaux -p ../../g-2_new_stat/ cC.20.48  -bin 50  -L 48  jack -mu 0.0020 || bad+=("mpi cB.20.48")
 fi
-./scale_setting jack ../../g-2_new_stat_try/jackknife/ ../../g-2_new_stat_try//fit_all/|| bad+=("scale setting 1")
+./scale_setting jack ../../g-2_new_stat/jackknife/ ../../g-2_new_stat_try//fit_all/|| bad+=("scale setting 1")
 
 
 ./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cA.53.24  -bin 50  -L 24  jack -mu 0.00530  0.010  0.020   0.26500   0.29000   0.30000 three_corr || bad+=("cA.53.24")
@@ -29,7 +29,7 @@ fi
 ./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cC.06.80  -bin 50  -L 80  jack -mu 0.00060  0.016  0.018   0.17500  0.19500  0.21500  0.0005850 0.00060 0.00060 three_corr || bad+=("cC.06.80")
 #./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cC.06.112 -bin 50  -L 112 jack -mu 0.00060  0.016  0.018   0.99999  0.99999  0.99999  three_corr|| bad+=("cC.06.112")
 ./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cC.06.112 -bin 50  -L 112 jack -mu 0.00060  0.016  0.018   0.18000  0.19000  0.20000  three_corr|| bad+=("cC.06.112")
-./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cD.54.96  -bin 50  -L 96  jack -mu 0.00054  0.013  0.014   0.16500  0.17500  0.17500  0.0004964 0.00054 0.00054 three_corr || bad+=("cD.54.96")  
+./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cD.54.96  -bin 50  -L 96  jack -mu 0.00054  0.013  0.014   0.16500  0.17000  0.17500  0.0004964 0.00054 0.00054 three_corr || bad+=("cD.54.96")  
 #./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cD.54.96  -bin 50  -L 96  jack -mu 0.00054  0.013  0.014   0.150  0.160  0.170  0.0004964 0.00054 0.00054 three_corr || bad+=("cD.54.96")  # from Ds
 ./gm2_analysis read_plateaux -p ../../g-2_new_stat/ cE.44.112 -bin 50  -L 112 jack -mu 0.00044  0.011  0.012   0.13000  0.14000  0.15000  three_corr || bad+=("cE.44.112")
 
@@ -85,6 +85,7 @@ fi
 ./some_custom_fits jack ../../g-2_new_stat/jackknife/ ../../g-2_new_stat//fit_all/  || bad+=("custom fit ")
 
 ./fit_strange jack ../../g-2_new_stat/jackknife/ ../../g-2_new_stat//fit_all_strange/ ||bad+=("fit strange ")
+./fit_charm jack ../../g-2_new_stat/jackknife/ ../../g-2_new_stat//fit_all_charm/ ||bad+=("fit charm ")
 
 if [[ -n "${bad-}" ]]; then
   echo -e "\n error in:\n"
