f637356f790d264754501d3ad9818e0d8dd443fa
Marcogarofalo <marcogarofalo31@gmail.com>
2026-08-19 16:18:54 +0200

    some printing

--- fit_charm.cpp
+++ fit_charm.cpp
@@ -735,20 +735,20 @@ int main(int argc, char** argv) {
                         ide++;
                     }
                 }
-                ide = 0;
-                for (int n = 0;n < fit_info.Nxen.size();n++) {
-                    for (int e : fit_info.Nxen[n]) {
-                        ide1 = 0;
-                        for (int n1 = 0;n1 < fit_info.Nxen.size();n1++) {
-                            for (int e1 : fit_info.Nxen[n1]) {
-                                printf("%-15g ",fit_info.cov[ide][ide1]/sqrt(fit_info.cov[ide][ide]*fit_info.cov[ide1][ide1]));
-                                ide1++;
-                            }
-                        }
-                        ide++;
-                        printf("\n");
-                    }
-                }
+                // ide = 0;
+                // for (int n = 0;n < fit_info.Nxen.size();n++) {
+                //     for (int e : fit_info.Nxen[n]) {
+                //         ide1 = 0;
+                //         for (int n1 = 0;n1 < fit_info.Nxen.size();n1++) {
+                //             for (int e1 : fit_info.Nxen[n1]) {
+                //                 printf("%-15g ",fit_info.cov[ide][ide1]/sqrt(fit_info.cov[ide][ide]*fit_info.cov[ide1][ide1]));
+                //                 ide1++;
+                //             }
+                //         }
+                //         ide++;
+                //         printf("\n");
+                //     }
+                // }
                 fit_info.compute_cov1_fit();
                 
                 // fit_info.covariancey = false;
@@ -778,12 +778,22 @@ int main(int argc, char** argv) {
                     count_aic++;
                 }
 
+                printf("%g   %g\n", myres->mean(amu_SD_l_common_a4.P[0]), myres->comp_error(amu_SD_l_common_a4.P[0]));
                 free_fit_result(fit_info, amu_SD_l_common_a4);  
                 // if (iW==1){                   
                 //     if (namefit == "amu_W_3b_BOS_BTM") {
                 //         std::cout << "Match found!" << std::endl;
                 //         exit(1);
                 //     }
+                // }
+                
+                // if (iW==5){                   
+                //     if (namefit == "amu_SDpWpLD_4b_onlyTM_alog3") {
+                //         std::cout << "Match found!" << std::endl;
+                //         exit(1);
+                //     }
+                //     printf("%s\n",namefit.c_str());
+                //     // exit(1);
                 // }
             }
         }
